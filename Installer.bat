e90410a5da29710c72d635ba4d1c3b746d83515e
Lavadragon15396 <taddufort@gmail.com>
2022-01-13 17:08:36 +0000

    add comments to installer

--- Installer.bat
+++ Installer.bat
@@ -1,8 +1,8 @@
 ::[Bat To Exe Converter]
 ::
-::fBE1pAF6MU+EWHreyHcjLQlHcDGROXmGIrAP4/z0/9alrkIeX6IVeYKb3L2CJfMvyEnrdp1t/HtX+A==
+::fBE1pAF6MU+EWHreyHcjLQlHcDGROXmGIrAP4/z0/9aErUkUWuUDWoPU2b/OCuEeqkPteZM+6lhWl8gHTDFZc3I=
 ::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFC5HSRa+GG6pDaET+NTZ4+WEqQ03Ve9/fobX36eyM+8G+EbhSYQoxXVPisgJHydRfR2lIAY3pg4=
+::fBw5plQjdCyDJGyX8VAjFC5HSRa+GG6pDaET+NT44O6OrEohdu4we4Sb8LWDYOYa5kD3SYUiw2hZnfceCQhXawCoaxwIoG1NuCqMNMj8
 ::YAwzuBVtJxjWCl3EqQJgSA==
 ::ZR4luwNxJguZRRnk
 ::Yhs/ulQjdF+5
@@ -32,12 +32,62 @@
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
+
+::   Colour code symbol : 
+::   STYLES:
+::   0m = Reset
+::   1m = Bold
+::   4m = Underline
+::   7m = Inverse
+::   
+::   NORMAL FOREGROUND COLORS:
+::   30m = Black 
+::   31m = Red
+::   32m = Green
+::   33m = Yellow
+::   34m = Blue
+::   35m = Magenta
+::   36m = Cyan
+::   37m = White
+::   
+::   NORMAL BACKGROUND COLORS:
+::   40m = Black
+::   41m = Red
+::   42m = Green
+::   43m = Yellow
+::   44m = Blue
+::   45m = Magenta
+::   46m = Cyan
+::   47m = White
+::   
+::   STRONG FOREGROUND COLORS:
+::   90m = White
+::   91m = Red
+::   92m = Green
+::   93m = Yellow
+::   94m = Blue
+::   95m = Magenta
+::   96m = Cyan
+::   97m = White
+::   
+::   STRONG BACKGROUND COLORS:
+::   100m = Black
+::   101m = Red
+::   102m = Green
+::   103m = Yellow
+::   104m = Blue
+::   105m = Magenta
+::   106m = Cyan
+::   107m = White
+
 @echo off
+::setup (set size and UDF 8 text encoding)
 TITLE Initializing . . .
 chcp 65001
 mode 95,30
 cls
 
+::check for admin
 net session >nul 2>&1
 if %errorLevel% == 0 (
     goto installer
@@ -45,6 +95,7 @@ if %errorLevel% == 0 (
     goto Not_Admin
 )
  
+:: no admin error
 :Not_Admin
     TITLE Reopen The installer
     echo  [91m█▀▀ █▀█ █▀█ █▀█ █▀█[0m
@@ -55,9 +106,11 @@ if %errorLevel% == 0 (
     echo [90mPress any key to exit . . .[0m
     PAUSE >nul
     exit
- 
+
+:: main code
 :installer
 cls
+:: title screen
 TITLE Block Jam Setup Wizard . . .
 echo             [91m██████╗ ██╗      █████╗  █████╗ ██╗  ██╗  [34m     ██╗ █████╗ ███╗   ███╗[0m
 echo             [91m██╔══██╗██║     ██╔══██╗██╔══██╗██║ ██╔╝  [34m     ██║██╔══██╗████╗ ████║[0m
@@ -77,6 +130,7 @@ echo                                   [90mPress any key to start
 echo                                           . . .[0m
 PAUSE >nul
 cls
+::welcome screen
 echo [91m██████╗ ██╗      █████╗  █████╗ ██╗  ██╗  [34m     ██╗ █████╗ ███╗   ███╗[0m
 echo [91m██╔══██╗██║     ██╔══██╗██╔══██╗██║ ██╔╝  [34m     ██║██╔══██╗████╗ ████║[0m
 echo [91m██████╦╝██║     ██║  ██║██║  ╚═╝█████═╝   [34m     ██║███████║██╔████╔██║[0m
@@ -90,10 +144,12 @@ echo Block Jam will now be installed in your program files folder
 echo [90mPresss any key to start . . .[0m
 PAUSE >nul
 cls
+:: copy files
 xcopy /I /E "%~dp0\Block Jam" "%ProgramFiles%\Block Jam"
 echo.
 echo Done copying!
 echo.
+:: shortcut choices
 :choice
 set /P c=Do you want to create a start menu folder [90m[Y/N][0m?  
 if /I "%c%" EQU "Y" goto :startFolder
@@ -106,6 +162,8 @@ if /I "%c%" EQU "N" goto :end
 goto :choice2
 
 :desktopShortcut
+
+::VBScripts for shortcuts
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
@@ -135,6 +193,8 @@ echo.
 echo Start menu shortcut created
 goto choice2
 
+
+::end
 :end
 echo.
 echo.
